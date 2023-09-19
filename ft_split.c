25118548cde37aba46cf0d513bfa2a913beea9af
Cesar De La Rosa Sobrino <cde-la-r@c3r10s6.42madrid.com>
2023-09-19 20:43:13 +0200

    split no norm, lstmap no strict

--- ft_split.c
+++ ft_split.c
@@ -6,82 +6,59 @@
 /*   By: cde-la-r <cde-la-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 21:21:30 by cde-la-r          #+#    #+#             */
-/*   Updated: 2023/09/19 20:23:04 by cde-la-r         ###   ########.fr       */
+/*   Updated: 2023/09/19 20:42:51 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	ft_count(const char *s, char c);
-static char		*ft_strndup(const char *s, size_t n);
-
-char	**ft_split(char const *s, char c)
+static int	count_words(const char *s, char c)
 {
-	size_t	i;
-	size_t	j;
-	size_t	l;
-	char	**r;
-
-	r = (char **)malloc((ft_count(s, c) + 1) * sizeof(char *));
-	if (!r)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s[i])
-	{
-		if (s[i] != c)
-		{
-			l = 0;
-			while (s[i + l] && s[i + l] != c)
-				l++;
-			r[j++] = ft_strndup(s + i, l);
-			i += l;
-		}
-		else
-			i++;
-	}
-	r[j] = NULL;
-	return (r);
-}
-
-static size_t	ft_count(const char *s, char c)
-{
-	size_t	count;
-	size_t	i;
+	int	count;
+	int	in_word;
 
 	count = 0;
-	i = 0;
-	while (s[i])
+	in_word = 0;
+	while (*s)
 	{
-		if (s[i] != c)
+		if (*s != c)
 		{
-			count++;
-			while (s[i] && s[i] != c)
-				i++;
+			if (!in_word)
+				count++;
+			in_word = 1;
 		}
 		else
-		{
-			while (s[i] && s[i] == c)
-				i++;
-		}
+			in_word = 0;
+		s++;
 	}
 	return (count);
 }
 
-static char	*ft_strndup(const char *s, size_t n)
+char	**ft_split(char const *s, char c)
 {
-	char	*dup;
-	size_t	i;
+	int			word_count = count_words(s, c);
+	char		**result = (char **)malloc((word_count + 1) * sizeof(char *));
+	int			i = 0;
+	const char	*start;
 
-	dup = (char *)malloc((n + 1) * sizeof(char));
-	if (!dup)
+	if (!result)
 		return (NULL);
-	i = 0;
-	while (i < n)
+	while (*s && i < word_count)
 	{
-		dup[i] = s[i];
-		i++;
+		while (*s == c)
+			s++;
+		start = s;
+		while (*s && *s != c)
+			s++;
+		result[i++] = ft_substr(start, 0, s - start);
+		if (!result[i - 1])
+		{
+			while (i > 0)
+				free(result[--i]);
+			free(result);
+			return (NULL);
+		}
 	}
-	dup[i] = '\0';
-	return (dup);
+	result[i] = NULL;
+	return (result);
 }
