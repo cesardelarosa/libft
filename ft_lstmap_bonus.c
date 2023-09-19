115e7ec364c621b10c55f526b0a0182e698ffee9
Cesar De La Rosa Sobrino <cde-la-r@c3r10s6.42madrid.com>
2023-09-19 20:25:04 +0200

    paco OK, paco -s KO split and lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,49 +6,36 @@
 /*   By: cde-la-r <cde-la-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 20:48:59 by cde-la-r          #+#    #+#             */
-/*   Updated: 2023/09/19 15:54:08 by cde-la-r         ###   ########.fr       */
+/*   Updated: 2023/09/19 20:07:34 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list *ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-    t_list *new_list = NULL;
-    t_list *new_tail = NULL;
+	t_list	*new_list;
+	t_list	*new_node;
+	void	*content;
 
-    while (lst)
-    {
-        void *content = f(lst->content);
-        if (!content)
-        {
-            // Si la función f devuelve NULL, considera liberar recursos si es necesario
-            if (del)
-                del(content);
-        }
-        else
-        {
-            // Crea un nuevo nodo para la nueva lista
-            t_list *new_node = ft_lstnew(content);
-            if (!new_node)
-            {
-                // Considera liberar recursos si no se pudo crear un nuevo nodo
-                if (del)
-                    del(content);
-            }
-            else
-            {
-                // Agrega el nuevo nodo a la nueva lista
-                if (!new_list)
-                    new_list = new_node;
-                else
-                    new_tail->next = new_node;
-                new_tail = new_node;
-            }
-        }
-
-        lst = lst->next;
-    }
-	return new_list;
+	new_list = NULL;
+	new_node = NULL;
+	while (lst)
+	{
+		content = f(lst->content);
+		if (!content)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		new_node = ft_lstnew(content);
+		if (!new_node)
+		{
+			ft_lstclear(&new_list, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_list, new_node);
+		lst = lst->next;
+	}
+	return (new_list);
 }
-
--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: cde-la-r <cde-la-r@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/07 12:16:45 by cde-la-r          #+#    #+#             */
-/*   Updated: 2023/09/19 19:19:55 by cde-la-r         ###   ########.fr       */
+/*   Updated: 2023/09/19 20:11:38 by cde-la-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	void	*r;
 
