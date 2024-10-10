f3609f7132a890a4ac7fe5095f050d60ca9415ee
cde-la-r <cde-la-r@student.42.fr>
2024-10-11 00:43:08 +0200

    split nice

--- libft.h
+++ libft.h
@@ -73,5 +73,7 @@ int		ft_putnbr(long n);
 int		ft_puthex(unsigned long n, int up);
 int		ft_putptr(void *ptr);
 int		ft_printf(const char *str, ...);
+int		ft_split_size(const char *s, char c);
+char	**ft_free_split(char **result);
 
 #endif 
